08011138e906a47c15cebd2e43828edd364880e7
kishan <kishan@sourcecodelab.co>
2026-02-22 03:13:06 +0530

    listing related code added

---
+++ controllers/category.controller.js
@@ -0,0 +1,73 @@
+const { Category, TRANSACTION_CATEGORIES } = require('../models/category.model');
+
+/**
+ * Get all categories
+ */
+const getCategories = async (req, res) => {
+  try {
+    const categories = await Category.find({ 
+      $or: [
+        { createdBy: req.user.id },
+        { createdBy: { $exists: false } } // System default categories if any
+      ]
+    });
+    
+    res.status(200).json({
+      categories,
+      constants: TRANSACTION_CATEGORIES
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+/**
+ * Create a new category
+ */
+const createCategory = async (req, res) => {
+  try {
+    const { name, type, icon } = req.body;
+    
+    const newCategory = new Category({
+      name,
+      type,
+      icon,
+      createdBy: req.user.id
+    });
+    
+    await newCategory.save();
+    res.status(201).json(newCategory);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+/**
+ * Update a category
+ */
+const updateCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, type, icon } = req.body;
+    
+    const category = await Category.findOneAndUpdate(
+      { _id: id, createdBy: req.user.id },
+      { name, type, icon },
+      { new: true, runValidators: true }
+    );
+    
+    if (!category) {
+      return res.status(404).json({ error: 'Category not found or unauthorized' });
+    }
+    
+    res.status(200).json(category);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  getCategories,
+  createCategory,
+  updateCategory
+};
